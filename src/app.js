d4a68a926e0b81a625bdd4fc4547f09da938cc97
OzielCosta-Dev <oziel19932016@outlook.com>
2026-05-29 11:25:42 -0300

    Add Codespaces Postgres setup

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import express from 'express'; // Importing the Express framework
+
+const app = express(); // Creating an instance of the Express application
+
+export default app; // Exporting the app instance for use in other files (like server.js)
---
+++ src/server.js
@@ -0,0 +1,3 @@
+import app from './app.js' // Importing the Express application instance from app.js
+
+app.listen(3001, () => console.log('Application is running at port 3001')) // Starting the server and listening on port 3001, with a callback to log a message when the server is running
