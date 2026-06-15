41aa27e8a90822cbe65c9f9a80e7749e38c6a036
OzielCosta-Dev <oziel19932016@outlook.com>
2026-06-15 16:33:48 -0300

    inserido-tela-register

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 import express from 'express'; // Importing the Express framework
 import routes from './routes.js'; // Importing the routes defined in routes.js
-import fileRouteConfig from './config/fileRoutes.cjs' // Importing the file route configuration for serving static files
+import cors from 'cors';
+import fileRouteConfig from './config/fileRoutes.cjs'; // Importing the file route configuration for serving static files
 
 const app = express(); // Creating an instance of the Express application
 
 app.use(express.json()); // Middleware to parse incoming JSON requests
 
+
+app.use(cors());
+
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded data (from forms)
-app.use('/product-file', fileRouteConfig) // Middleware to serve static files from the uploads directory when accessed via /product-file
-app.use('/category-file', fileRouteConfig) // Middleware to serve static files from the uploads directory when accessed via /product-file
+app.use('/product-file', fileRouteConfig); // Middleware to serve static files from the uploads directory when accessed via /product-file
+app.use('/category-file', fileRouteConfig); // Middleware to serve static files from the uploads directory when accessed via /product-file
 
 app.use(routes); // Using the imported routes in the Express application
 
